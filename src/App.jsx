3f1b4e032e10218e329013fca50dffc7c9937d39
Noble Benjamin <nobleben2008@gmail.com>
2024-07-21 11:34:22 +0100

    Made the onChange handlers

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,36 @@ function App() {
   const [toValue, setToValue] = useState('USD')
   const [fromValue, setFromValue] = useState('USD')
 
+  const handleToValueChange = (e) => {
+    setToValue(e.target.value)
+  }
+
+  const handleFromValueChange = (e) => {
+    setFromValue(e.target.value)
+  }
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value)
+  }
+
 
   return (
     <div className='App'>
-      <input type="text" name="amount" id="amount" />
-      <select name="from" value='USD'>
+      <input type="text" name="amount" id="amount" value={amount} onChange={handleAmountChange}/>
+      <select name="from" value={fromValue} onChange={handleFromValueChange}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select name="to" value='USD'>
+      <select name="to" value={toValue} onChange={handleToValueChange}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
 
-      <p>AMOUNT</p>
+      <p>{convertedAmount}</p>
     </div>
   )
 }
